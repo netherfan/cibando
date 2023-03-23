d7492b92e1eec62daf3183ad1becddc608839620
Rocco Costantino <roccostalternative@gmail.com>
2023-03-23 12:28:09 +0100

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,10 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 export class AppComponent {
   title = 'cibando';
-  ciao = 'red';
 
-  images = [
-    {id: 1,
-      label: "Spaghetti al pomodoro"},
-    {id: 2,
-      label: "Tagliata di manzo"},
-    {id: 3,
-      label: "Tiramisù"}
-  ];
+  evidenziato = true;
 
-  percorso = "../assets/images/carousel-";
-  // percorsostd = "../assets/images/carousel-1.jpg"
-
-  colore: 'green';
+  onEvidenziazione(){
+    this.evidenziato = !this.evidenziato;
+  }
 }
